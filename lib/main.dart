8df8e6b39a6f63f0e42aaa9c8b745274fd761b17
Dylan J. Gerrits <dyrits@outlook.com>
2024-01-09 15:06:59 +0100

    Challenge completed

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,27 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  color: Colors.red,
+                  width: 100,
+                ),
+                Container(
+                  color: Colors.yellow,
+                  width: 100,
+                  height: 100
+                ),
+                Container(
+                  color: Colors.blue,
+                  width: 100,
+                ),
+              ],
+            ),
+          )),
     );
   }
 }

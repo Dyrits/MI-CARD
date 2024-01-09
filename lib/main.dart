0fb4a56910584187808339c0a624b0ad1aada2a2
Dylan J. Gerrits <dyrits@outlook.com>
2024-01-09 15:31:54 +0100

    Imported and used fonts from Google

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,32 @@ class App extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Center(
+            child: Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+              const CircleAvatar(radius: 50.0, backgroundImage: AssetImage("assets/photos/dylan-j-gerrits.jpg")),
+              const Text(
+                "Dylan J. Gerrits",
+                style: TextStyle(
+                  fontFamily: "Pacifico",
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "FULL-STACK DEVELOPER",
+                style: TextStyle(
+                  fontFamily: "Source Sans",
+                  fontSize: 20,
+                  color: Colors.teal[100],
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                ),
+              ),
+            ]),
+          ),
+        ),
       ),
     );
   }
